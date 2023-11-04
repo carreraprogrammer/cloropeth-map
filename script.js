1021249c5ec7d775e6fae82a9888e40a70be161d
Daniel <carreraprogrammer@gmail.com>
2023-11-04 10:38:45 -0500

    Create variables and functions

---
+++ script.js
@@ -0,0 +1,11 @@
+let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+let educationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+
+let countyData
+let educationData
+
+let canvas = d3.select('#canvas')
+
+let drawMap = () => {
+ 
+}
